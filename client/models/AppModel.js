198a8ef41d9471f9206ceebe96bf379db73d1bb7
Sean <dokko1230@gmail.com>
2014-04-30 17:29:22 -0700

    adds event handler for playFirst on SongQueues'

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -12,13 +12,22 @@ var AppModel = Backbone.Model.extend({
     end up refering to the window. That's just what happens with all JS events. The handlers end up
     getting called from the window (unless we override it, as we do here). */
 
+    // app event listeners
+    // on songQueue
+    this.get('songQueue').on('playFirst', function(songQueue) {
+      this.set('currentSong', songQueue.at(0));
+    }, this);
 
+    // on songs
     params.library.on('play', function(song){
       this.set('currentSong', song);
     }, this);
     params.library.on('enqueue', function(song){
       this.get('songQueue').add(song);
     }, this);
+    params.library.on('dequeue', function(song) {
+      this.get('songQueue').shift();
+    });
   }
 
 });
